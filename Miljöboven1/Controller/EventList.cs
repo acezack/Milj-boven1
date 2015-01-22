3b9fed3956df183f97305dd2b9c1531cc1104f66
acezack <kal.lal@hotmail.com>
2015-01-22 11:27:44 +0100

    asd

---
+++ Miljöboven1/Controller/EventList.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Miljöboven.View;
+
+namespace Miljöboven.Controller
+{
+    public class EventList
+    {
+        List<Event> eventList;
+        HandläggareForm handläggareForm;
+        public EventList(HandläggareForm handläggareForm) 
+        {
+            this.handläggareForm = handläggareForm;
+            this.eventList = new List<Event>();
+            AddPreviousItemsToList();
+        }
+        void AddPreviousItemsToList()
+        {
+            Event ev = new Event("Började projektet");
+            eventList.Add(ev);
+        }
+        public void AddToList(Event ev)
+        {
+            eventList.Add(ev);
+        }
+        public string GetEventInfo(int id)
+        {
+            string svar = eventList[id].Datum + "   " + eventList[id].Kommentar;
+            return svar;
+        }
+        public int getCount()
+        {
+            return eventList.Count();
+        }
+        public string getEventInfo(int id)
+        {
+            return eventList[id].Datum + "   " + eventList[id].Kommentar;
+        }
+    }
+}
---
+++ Miljöboven1/Controller/UserList.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Miljöboven.Controller
+{
+    public class UserList
+    {
+        List<User> userList = new List<User>();
+
+        public void Add(User user)
+        {
+            userList.Add(user);
+        }
+        public int GetCount()
+        {
+            return userList.Count();
+        }
+        public string GetUserName(int index)
+        {
+            return userList[index].UserName;
+        }
+        public string GetPassword(int index)
+        {
+            return userList[index].Password;
+        }
+        public int GetType(int index)
+        {
+            return userList[index].Type;
+        }
+        public int GetID(int index)
+        {
+            return userList[index].ID;
+        }
+    }
+}
